d7afc2dff9eea8a120dfe0bc55912e00b0c563f6
Kiririn00 <Varit@Varits-MacBook-Pro.local>
2017-05-16 18:35:50 +0900

    no message

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,5 +1,5 @@
 /**
- * UserController ching
+ * UserController
  *
  * @description :: Server-side logic for managing Users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers

a488885eb60917ab74557ee78b5bf2fef376567b
Kiririn00 <Varit@Varits-MacBook-Pro.local>
2017-06-14 22:18:25 +0900

    upload SQL file

--- SQL/single_dog.sql
+++ SQL/single_dog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 10, 2017 at 02:11 PM
+-- Generation Time: Jun 14, 2017 at 03:13 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.5.38
 
@@ -19,6 +19,79 @@ SET time_zone = "+00:00";
 --
 -- Database: `single_dog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answerlog`
+--
+
+CREATE TABLE `answerlog` (
+  `answer_log_id` int(10) UNSIGNED NOT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `value` float DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `category_name` varchar(45) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `question_id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `question_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `question_name` varchar(200) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`question_id`, `category_id`, `question_type_id`, `user_id`, `question_name`, `createdAt`, `updatedAt`) VALUES
+(1, 2, 1, 1, 'How are you doing?', '2017-06-05 19:19:39', '2017-06-05 19:19:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questiontype`
+--
+
+CREATE TABLE `questiontype` (
+  `question_type_id` int(10) UNSIGNED NOT NULL,
+  `question_type_name` varchar(200) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questiontype`
+--
+
+INSERT INTO `questiontype` (`question_type_id`, `question_type_name`, `createdAt`, `updatedAt`) VALUES
+(1, 'user', NULL, NULL),
+(2, 'admin', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -43,11 +116,36 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `age`, `gender`, `postcode`, `email`, `createdAt`, `updatedAt`) VALUES
-(1, 'endopasmic', '00236263', 1, 1, 5250066, 'varit.asawavetvutt@gmail.com', '2017-05-10 19:38:39', '2017-05-10 19:38:39');
+(1, 'endopasmic', '00236263', 1, 1, 5250066, 'varit.asawavetvutt@gmail.com', '2017-05-10 19:38:39', '2017-05-10 19:38:39'),
+(2, 'endopasmic', '00236263', 1, 1, 5250066, 'varit.asawavetvutt@gmail.com', '2017-05-12 18:23:16', '2017-05-12 18:23:16');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `answerlog`
+--
+ALTER TABLE `answerlog`
+  ADD PRIMARY KEY (`answer_log_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `questiontype`
+--
+ALTER TABLE `questiontype`
+  ADD PRIMARY KEY (`question_type_id`);
 
 --
 -- Indexes for table `user`
@@ -60,10 +158,30 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `answerlog`
+--
+ALTER TABLE `answerlog`
+  MODIFY `answer_log_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `question_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `questiontype`
+--
+ALTER TABLE `questiontype`
+  MODIFY `question_type_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

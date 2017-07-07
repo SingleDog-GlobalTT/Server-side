f80b5d31feb1d977d8a297c22f46c82e931c8580
Kiririn00 <Varit@Varits-MacBook-Pro.local>
2017-07-07 16:16:32 +0900

    done Match system

--- api/controllers/MatchingController.js
+++ api/controllers/MatchingController.js
@@ -3,6 +3,7 @@
  */
 
 var async = require('async');
+var unique = require('array-unique');
 
 module.exports = {
 
@@ -64,32 +65,50 @@ module.exports = {
 
       }//end func
 
-      function answerRecord(category_value, callback) {
+      function answerRecord(category_value, answer_query,callback) {
+
+        console.log("answer_query: ", answer_query);
+
+        AnswerLog.create(answer_query, function (err, created) {
+
+          if(err){console.log(err);}
+
+          console.log(created);
+
+          //callback(null, category_value);
+
+        });
+
+      }//end func
+
+      /*
+      * feature: define query value
+      * */
+      function defineAnswerLog(category_value, callback) {
+
+        var answer_query = [];
 
         for(var i=0; i< 4; i++) {
 
-          console.log("category_value", category_value);
-
-          var answer_query = {
+          answer_query.push({
             category_id: i+1,
             user_id: user_id,
             value: category_value[i]/5*100
-          };
-
-          console.log("answer_query: ", answer_query);
-
-
-          AnswerLog.create(answer_query).exec(function (err, created) {
-            console.log(created);
-            callback(null, category_value);
           });
 
-        }
-      }//end func
+        }// end loop
+
+        AnswerLog.create(answer_query, function (err, created) {
+          console.log(created);
+        });
+
+        callback(null, category_value, answer_query);
+
+      }
 
       async.waterfall([
         answerCalculate,
-        answerRecord
+        defineAnswerLog
       ], function (err,answer_list) {
         return res.json({
           answer_list: answer_list
@@ -129,7 +148,6 @@ module.exports = {
           var category_value_num = category_value.length,
             value = [];
 
-
           if(err){
             console.log(err);
           }
@@ -158,16 +176,13 @@ module.exports = {
 
           if(err){console.log(err);}
 
-          console.log("other_category_value", other_category_value);
-
           //make value array
           for(var i=0; i<other_category_value_num; i++){
             other_value.user_id.push(other_category_value[i].user_id);
             other_value.value.push(other_category_value[i].value);
           }
 
-          callback(current_user_category_value);
-
+          callback(null, current_user_category_value, other_value);
 
         });
 
@@ -175,16 +190,40 @@ module.exports = {
 
       function findMatchUser(current_user_category_value, other_user_category_value, callback) {
 
+        console.log("current user: ", current_user_category_value);
+        console.log("other user: ", other_user_category_value);
+
+        var matching_range = 5,
+          other_user_length = other_user_category_value.user_id.length,
+          match_user = [];
+
+
+        for(var i=0; i<other_user_length; i++){
+
+          var matching_value = current_user_category_value[i]-other_user_category_value.value[i];
+
+          console.log(matching_value, matching_range);
+
+            if( matching_value <= Math.abs(matching_range)){//match
+              match_user.push(other_user_category_value.user_id[i]);
+            }
+
+        }
+
+        var unique_match_user = unique(match_user);
+
+        callback(null, unique_match_user);
+
       }//end func
 
       async.waterfall([
         findCurrentUserCategoryLog,
         findOtherUserCategoryLog,
         findMatchUser
-      ], function (err, result) {
+      ], function (err, match_user) {
 
           return res.json({
-            status: result
+            answer: match_user
           });
 
       });
